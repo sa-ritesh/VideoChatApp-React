0bc04687a5a2c0f5bd0168a9d48bf34ca93a85b0
Ritesh Arora <67755113+sa-ritesh@users.noreply.github.com>
2024-08-14 11:54:52 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const io = require("socket.io")(server, {
   },
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
 
